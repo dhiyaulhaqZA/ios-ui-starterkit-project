54317d2e50211a38fe1d6a0374960eca75327d50
Dhiya Ulhaq Zulha Alamsyah <dhiya.alamsyah@students.amikom.ac.id>
2020-06-13 13:47:35 +0700

    Initial Commit

---
+++ StarterTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarterTemplate
+//
+//  Created by Dhiya Ulhaq Zulha Alamsyah on 13/06/20.
+//  Copyright © 2020 dhiyaulhaqza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
